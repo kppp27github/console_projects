cc4025f3c18f51c1afa2e5524b66498469d6c518
kppp27github <mail2kppp27@gmail.com>
2025-11-16 00:00:31 +0530

    Started coding on Calculator(python) on 16-11-2025

---
+++ python/calculator/main.py
@@ -0,0 +1,24 @@
+"""
+Project Name: Calculator;
+Programmer: Koushik Saha;
+Date: 16-11-2025;
+"""
+
+
+class Calculator:
+    def __init__(self) -> None:
+        print("\nC A L C U L A T O R")
+        print("\t--Programmed by kppp27")
+        print('# Type "exit" to close this program.\n')
+        self.user_input()
+
+    def user_input(self) -> None:
+        self.input_text: str = input(";\t")
+        if self.input_text != "exit":
+            print("\t\t", self.input_text)
+            self.user_input()
+        else:
+            return
+
+
+calc: Calculator = Calculator()
