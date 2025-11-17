6666d40096fa872f07d971b7dec1f71d1b977ba1
kppp27github <101812340+kppp27github@users.noreply.github.com>
2025-11-17 17:22:35 +0530

    Python calculator exception modified on 17-11-2025

--- python/calculator/main.py
+++ python/calculator/main.py
@@ -6,22 +6,25 @@ Date: 16-11-2025;
 
 
 class Calculator:
-    def __init__(self) -> None:
-        print("\nC A L C U L A T O R")
-        print("\t--Programmed by kppp27")
-        print('# Type "exit" to close this program.\n')
-        self.user_input()
 
-    def user_input(self) -> None:
-        self.input_text: str = input("; ")
-        if self.input_text != "exit":
-            try:
-                print("\t", eval(self.input_text))
-            except (NameError, SyntaxError) as e:
-                print(e)
-            self.user_input()
-        else:
-            return
+  def __init__(self) -> None:
+    print("\nC A L C U L A T O R")
+    print("\t--Programmed by kppp27")
+    print('# Type "exit" to close this program.\n')
+    self.user_input()
+
+  def user_input(self) -> None:
+    self.input_text: str = input("; ")
+    if self.input_text != "exit":
+      try:
+        print("\t", eval(self.input_text))
+      except (NameError) as e:
+        print(e)
+      except (SyntaxError) as e:
+        print("Unknown syntax")
+      self.user_input()
+    else:
+      return
 
 
 calc: Calculator = Calculator()
