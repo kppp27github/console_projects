5b14a4d1edb8da06812c6eb2937672936aceb138
kppp27github <101812340+kppp27github@users.noreply.github.com>
2025-11-17 18:23:30 +0530

    Program: SecretMessage initialized with few codes on 17-11-2025

---
+++ python/secret_message/main.py
@@ -0,0 +1,40 @@
+"""
+Program Name: [SecretMessage];
+Programmer: [Koushik Saha];
+Date: [17-11-2025]
+"""
+
+
+class SecretMessage:
+
+  def __init__(self) -> None:
+    print("\n[ S e c r e t M e s s a g e ]\n")
+    print("\t-Programmer: Koushik Saha")
+    print("\n# Type \"exit\" to close from this program.\n")
+    print("-> Options: \n")
+    print("1. Encode Message")
+    print("2. Decode Message")
+    self.options()
+
+  def options(self) -> None:
+    self.selected_option: str = input("\nSelect an option: ")
+    match self.selected_option:
+      case "1":
+        self.encode_message()
+      case "2":
+        self.decode_message()
+      case "exit":
+        return
+      case _:
+        print("\n[Error: Invalid selection! Try again.]\n")
+        self.options()
+
+  def encode_message(self) -> str:
+    return ""
+
+  def decode_message(self) -> str:
+    return ""
+
+
+sm: SecretMessage = SecretMessage()
+
