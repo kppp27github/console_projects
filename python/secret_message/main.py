ea2932ba9b7eef93d5736f38fa22836de9b38870
kppp27github <mail2kppp27@gmail.com>
2025-11-19 01:33:08 +0530

    Modified on SecretMessage(python) on 20-11-2025

--- python/secret_message/main.py
+++ python/secret_message/main.py
@@ -7,34 +7,34 @@ Date: [17-11-2025]
 
 class SecretMessage:
 
-  def __init__(self) -> None:
-    print("\n[ S e c r e t M e s s a g e ]\n")
-    print("\t-Programmer: Koushik Saha")
-    print("\n# Type \"exit\" to close from this program.\n")
-    print("-> Options: \n")
-    print("1. Encode Message")
-    print("2. Decode Message")
-    self.options()
-
-  def options(self) -> None:
-    self.selected_option: str = input("\nSelect an option: ")
-    match self.selected_option:
-      case "1":
-        self.encode_message()
-      case "2":
-        self.decode_message()
-      case "exit":
-        return
-      case _:
-        print("\n[Error: Invalid selection! Try again.]\n")
+    def __init__(self) -> None:
+        print("\n[ S e c r e t M e s s a g e ]\n")
+        print("\t-Programmer: Koushik Saha")
+        print('\n# Type "exit" to close from this program.\n')
+        print("-> Options: \n")
+        print("1. Encode Message")
+        print("2. Decode Message")
         self.options()
 
-  def encode_message(self) -> str:
-    return ""
+    def options(self) -> None:
+        self.selected_option: str = input("\nSelect an option: ")
+        match self.selected_option:
+            case "1":
+                print(f"\n[Encoded Message]: {self.encode_message()}")
+            case "2":
+                self.decode_message()
+            case "exit":
+                return
+            case _:
+                print("\n[Error]: Invalid selection! Try again.")
+                self.options()
 
-  def decode_message(self) -> str:
-    return ""
+    def encode_message(self) -> str:
+        msg: str = input("\nMessage: ")
+        return msg
+
+    def decode_message(self) -> str:
+        return ""
 
 
 sm: SecretMessage = SecretMessage()
-
